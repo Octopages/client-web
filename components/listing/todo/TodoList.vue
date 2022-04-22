78a74adf383a36c61ba20007387745ec0a1b3323
Amr <muhammad.amro.moussa@gmail.com>
2022-04-22 06:30:14 +0200

    feat: add todo list

---
+++ components/listing/todo/TodoList.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-card>
+    <v-card-title>
+      <slot name="title" :tasks="todoList">Default Title</slot>
+    </v-card-title>
+    <v-card-text>
+      <v-data-table :headers="headers" :items="todoList">
+        <div slot="item.done" slot-scope="{ item }">
+          <v-switch v-model="item.done" />
+        </div>
+      </v-data-table>
+    </v-card-text>
+  </v-card>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      headers: [
+        { text: 'Task Name', value: 'name' },
+        { text: 'Done', value: 'done', sortable: false },
+      ],
+      todoList: [
+        { name: 'Feed The Dog', done: true },
+        { name: 'Get Married', done: false },
+        { name: 'Eat Food', done: false },
+      ],
+    }
+  },
+}
+</script>
---
+++ pages/todo.vue
@@ -0,0 +1,16 @@
+<template>
+  <todo-list>
+    <span slot="title" slot-scope="{ tasks }">
+      To Do Tasks: {{ tasks.length }}
+    </span>
+  </todo-list>
+</template>
+
+<script>
+import TodoList from '~/components/listing/todo/TodoList.vue'
+export default {
+  components: {
+    TodoList,
+  },
+}
+</script>
