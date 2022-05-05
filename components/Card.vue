523575a4e476cfa2da81dc75f9fd92459cfe5744
Amr <muhammad.amro.moussa@gmail.com>
2022-05-05 04:22:54 +0200

    feat: add slots and props to cards

---
+++ components/Card.vue
@@ -0,0 +1,39 @@
+<template>
+  <div :style="{ backgroundColor: color }" class="card">
+    <slot name="title">
+      <div>{{ title }}</div>
+    </slot>
+
+    <slot name="body">
+      <div>{{ body }}</div>
+    </slot>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    color: {
+      type: String, // Number , Boolean , Array , Object , Function
+      default: 'red',
+      // required: true,
+      // validate(val) { return true if valid }
+    },
+    title: {
+      type: String,
+      default: 'Title',
+    },
+    body: {
+      type: String,
+      default: 'Body',
+    },
+  },
+}
+</script>
+<style scoped>
+.card {
+  padding: 10px;
+  border-radius: 5px;
+  margin: 5px;
+}
+</style>
---
+++ components/VueSlots.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <Card color="blue" title="hello" body="Hello World">
+      <div slot="title">Title 1 From Slot</div>
+
+      <div slot="body">
+        Body 1 From Slot
+        <v-text-field placeholder="Ay 7aga 3ashan e7na gamdeen" />
+      </div>
+    </Card>
+
+    <Card color="red" title="Title 2" body="Body 2" />
+    <Card color="green" title="Title 3" body="Body 3" />
+    <Card color="brown" />
+  </div>
+</template>
+<script>
+import Card from '~/components/Card.vue'
+export default {
+  components: { Card },
+}
+</script>
